b83b64334b8998b2acd78452637b638b82541c35
alexander <alexandernyaga1999@gmail.com>
2023-10-24 12:39:24 +0300

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    // let name = 'mario';
+
+    const handleClick = () => {
+        name = 'luiji'
+    }
+    return ( 
+        <div className="home">
+            <h2>Homepage</h2>
+            <p>{ name }</p>
+            <button onClick={handleClick }>Click me</button>
+        </div>
+     );
+} 
+ 
+export default Home;
