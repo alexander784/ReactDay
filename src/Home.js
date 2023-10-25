736477df9390f3e9376dcb2ba76a44f10a18baa7
alexander <alexandernyaga1999@gmail.com>
2023-10-25 16:23:47 +0300

    loop over the blog

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,28 @@
+import { useState } from "react";
 const Home = () => {
-    // let name = 'mario';
-
-    const handleClick = () => {
-        // name = 'luiji'
-    }
+    const [blogs, setblogs,] = useState([
+        {title:'My new website', body: 'Lorem ipsum...', author: 'mario', id:1 },
+        {title:'Welcome my party', body: 'Lorem ipsum...', author: 'lex', id:2 },
+        {title:'Web dev top tips', body: 'Lorem ipsum...', author: 'james', id:3 },
+    ]);
+    
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name }</p>
-            <button onClick={handleClick }>Click me</button>
-        </div>
-     );
-} 
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h1>{blog.title}</h1>
+                    <p>Written by {blog.author}</p>
+
+                    </div>
+            ))}
+            </div>
+    );
+            }
+
+
+            
+            
+     
+
  
 export default Home;
