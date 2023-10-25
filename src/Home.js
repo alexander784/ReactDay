f47069b4c0d1761cbb25153d8e595e49d08c05a4
alexander <alexandernyaga1999@gmail.com>
2023-10-25 17:33:06 +0300

    Pass functions into other componets as props

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
+import Bloglist from "./Bloglist";
 const Home = () => {
     const [blogs, setblogs,] = useState([
         {title:'My new website', body: 'Lorem ipsum...', author: 'mario', id:1 },
         {title:'Welcome my party', body: 'Lorem ipsum...', author: 'lex', id:2 },
-        {title:'Web dev top tips', body: 'Lorem ipsum...', author: 'james', id:3 },
+        {title:'Web dev top tips', body: 'Lorem ipsum...', author: 'mario', id:3 },
     ]);
+
+    const handleDelete = (id) => {
+         const newBlogs = blogs.filter(blog => blog.id !==  id);
+         setblogs(newBlogs);
+    }
     
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h1>{blog.title}</h1>
-                    <p>Written by {blog.author}</p>
-
-                    </div>
-            ))}
+            <Bloglist  blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
             </div>
     );
             }
