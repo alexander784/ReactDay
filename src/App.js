c42410473694a911f1ad6545404e81fe17761cb7
alexander <alexandernyaga1999@gmail.com>
2023-11-06 14:01:43 +0300

    ReactRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import logo from './logo.svg';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   const title = 'welcome to the new blog'
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className='Content'>
-        <Home />
+         
+         {/* Make sure only one Route shows at any one time  */}
+        <Switch>
+        {/* path for Homepage */}
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
       </div>
       
     </div>
+    </Router>
   );
 }
 
