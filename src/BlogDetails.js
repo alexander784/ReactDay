ff0d6bfe54999f82116476367e72631c4f412772
alexander <alexandernyaga1999@gmail.com>
2023-11-18 11:55:53 +0300

    delete blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,22 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
-const BlogDetails = () => {
+const BlogDetails = () => { 
 
     const { id } = useParams();
-    const [ data: blog, error, isPending ] = useEffect('')
+    const [ data: blog, error, isPending ] = useFetch('')
+    const history = useHistory();
+
+    const handleClick = () {
+        fetch('http://localhost:6001' + blog.id, {
+            method: 'DELETE'
+        } ).then(() => {
+            history.push('/');
+
+        })
+
+    }
     return ( 
         <div className="blog-details">
             { isPending && <div>Loading...</div>}
@@ -14,6 +26,7 @@ const BlogDetails = () => {
                 <h2>{ blog.title }</h2>
                 <p>Written by { blog.author }</p>
                 <div>{ blog.body }</div>
+                <button onClick={}>Delete</button>
                 </article>
             )}
         </div>
